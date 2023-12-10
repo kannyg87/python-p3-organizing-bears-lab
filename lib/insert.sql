6ada2ec31beb78c4c6721901acb71c2eaf7dfb95
Kanny <kanny.ghafour87@gmail.com>
2023-12-10 11:45:00 -0600

    lab completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Mr. Chocolate', 'M', 5, 'Brown', 'Calm', TRUE);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Rowdy', 'M', 3, 'Black', 'Playful', TRUE);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Tabitha', 'F', 4, 'Golden', 'Shy', TRUE);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Sergeant Brown', 'M', 7, 'Dark Brown', 'Serious', TRUE);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Melissa', 'F', 2, 'Light Brown', 'Curious', TRUE);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Grinch', 'M', 6, 'Green', 'Grumpy', FALSE);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Wendy', 'F', 5, 'White', 'Friendly', TRUE);
+INSERT INTO bears (sex, age, color, temperament, alive) VALUES ('F', 3, 'Grey', 'Mysterious', TRUE);
+
